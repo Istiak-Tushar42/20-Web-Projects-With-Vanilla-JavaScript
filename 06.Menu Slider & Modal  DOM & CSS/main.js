72aefe490b128133cab2e231d51f76bb97d41252
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2020-08-21 23:13:28 +0600

    Starting Draw Hangman With SVG Project

--- 06.Menu Slider & Modal  DOM & CSS/main.js
+++ 06.Menu Slider & Modal  DOM & CSS/main.js
@@ -22,3 +22,19 @@ close.addEventListener('click', () => {
 window.addEventListener('click', e => {
     e.target == modal ? modal.classList.remove('show-modal') : false;
 });
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', e => {
+    const error = document.querySelector('.error');
+
+    e.preventDefault();
+
+    if(name.value.trim() == '' ) {
+        error.innerText = 'Must fill this field';
+    }
+});
